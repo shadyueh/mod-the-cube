9a7d13a5da939a5ef65d6d304ab698f603d7a214
Held Grijo <held.grijo@gmail.com>
2022-01-13 04:28:50 -0400

    feat: Inputs for change position, scale
    and rotation speed

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -1,24 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+
+    Slider positionSlider, rotationSlider, scaleSlider;
+    float moveSpeed = 10f;
+    private Vector3 targetPosition, targetRotation, targetScale;
+
     void Start()
     {
         transform.position = new Vector3(0,0,0);
-        transform.localScale = Vector3.one * 1.3f;
+        transform.localScale = Vector3.one;
         
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
 
+        positionSlider =  GameObject.Find("Position").GetComponentInChildren<Slider>();
+        rotationSlider = GameObject.Find("RotationSpeed").GetComponentInChildren<Slider>();
+        scaleSlider = GameObject.Find("Scale").GetComponentInChildren<Slider>();
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        targetRotation = Vector3.right;
+
+        transform.Rotate(targetRotation * rotationSlider.value * Time.deltaTime);
+
+        targetPosition  = Vector3.right * positionSlider.value;
+        targetScale = Vector3.one * scaleSlider.value;
+
+        Debug.Log(rotationSlider.value);
+    }
+
+    private void FixedUpdate()
+    {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.fixedDeltaTime);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, moveSpeed * Time.fixedDeltaTime);
+
     }
 }
